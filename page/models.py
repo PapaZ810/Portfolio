0a6eeab4a9c9942a8c5ddbfbde505bcfb081f797
Zac Durham <43076672+PapaZ810@users.noreply.github.com>
2024-09-06 22:45:14 -0600

    cards working

--- page/models.py
+++ page/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 
 class Project(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=50)
+    short_description = models.CharField(max_length=100, default='Short description')
     text = models.TextField()
 
     def __str__(self):
