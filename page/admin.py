c00fcbdd899b82bdb1cf8c01b73fa1da224d4092
Zac Durham <43076672+PapaZ810@users.noreply.github.com>
2024-09-17 08:58:59 -0600

    change color of project hr and add short_description to admin

--- page/admin.py
+++ page/admin.py
@@ -5,6 +5,14 @@ from .models import *
 admin.site.site_header = 'Portfolio Admin Panel'
 admin.site.site_title = 'Portfolio Admin Panel'
 
-admin.site.register(Project)
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['title', 'short_description']
+    search_fields = ['title', 'short_description']
+    list_filter = ['title', 'short_description']
+    ordering = ['title', 'short_description']
+
+
+admin.site.register(Project, ProjectAdmin)
 admin.site.register(Home)
 admin.site.register(Skill)
